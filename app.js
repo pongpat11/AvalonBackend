c25450bee26d3be385d0951d64d7ad9d339e9be5
neconone <p.chaiolan30@gmail.com>
2019-11-12 01:15:23 +0700

    regiter api

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv/config');
 
+// Prevent collection name end with 's'
+mongoose.pluralize(null);
+
 // Middleware for route
 const app = express();
 const routes = require('./routes/routes');
@@ -10,7 +13,7 @@ app.use(bodyParser.json());
 app.use('/', routes);
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, (err, res) => {
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
     if(err) {
         console.log(err);
     }
--- routes/routes.js
+++ routes/routes.js
@@ -6,16 +6,18 @@ router.get('/login', (req, res) => {
     res.send('Hello world');
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const user = new User({
         email: req.body.email,
         password: req.body.password,
         name: req.body.name
     });
-    user.save((err) => {
-        if(err) res.json({message: err});
-        else res.json({message: success});
-    })
+    try {
+        const regUser = await user.save();
+        res.json({message: 'success'});
+    } catch (err) {
+        res.json({message: err});
+    }
 })
 
 module.exports = router;
