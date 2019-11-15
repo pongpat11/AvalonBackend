2987e1626c9ac0dac54015517c2f4ef14d18fdd9
F9Uf <atoseka@gmail.com>
2019-11-15 20:34:28 +0700

    enable cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 // Prevent collection name end with 's'
@@ -12,6 +13,7 @@ mongoose.set('useCreateIndex', true)
 // Middleware for route
 const app = express();
 const routes = require('./routes/routes');
+app.use(cors())
 app.use(bodyParser.json());
 app.use('/', routes);
 
