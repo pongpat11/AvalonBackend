e3e7effcae804eb791717c3343ab5b058432cf02
neconone <p.chaiolan30@gmail.com>
2019-11-20 22:28:09 +0700

    Delete test

--- socket/socket.js
+++ socket/socket.js
@@ -3,10 +3,6 @@ const lobby = require('./lobby');
 
 module.exports = (server) => {
     const io = socketIO.listen(server);
-    const test = require('./test');
-    const test2 = require('./test2');
-    test(io);
-    test2(io);
     lobby(io);
 }
 
