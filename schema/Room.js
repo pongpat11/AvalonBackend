f2d0b93ccd58364f2dbcc2149b66d267d8ef17f0
neconone <p.chaiolan30@gmail.com>
2019-11-22 20:05:14 +0700

    Room schema update

--- schema/Room.js
+++ schema/Room.js
@@ -3,19 +3,26 @@ const mongoose = require('mongoose');
 const RoomSchema = mongoose.Schema({
     roomName: {
         type: String,
-        required: true
+        required: true,
     },
     password: {
-        type: String
-    },
-    playerInRoom: [{
-        userName: String,
-        role: Number
-    }],
-    roomStatus: {
         type: String,
-        required: true
-    }
-});
+        required: false
+    },
+    leader: {
+        type: String,
+        required: true,
+    },
+    player: [{
+        playerName: {
+            type: String,
+            required: true
+        },
+        playerRole: {
+            type: String,
+            required: false
+        }
+    }]
+})
 
 module.exports = mongoose.model('room', RoomSchema);
