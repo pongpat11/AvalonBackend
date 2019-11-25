8c0f0bb7f60019c7515ee6c41e7f2d437e0961e9
neconone <p.chaiolan30@gmail.com>
2019-11-25 16:11:38 +0700

    user

--- routes/routes.js
+++ routes/routes.js
@@ -23,7 +23,7 @@ router.post('/register', [
 ], register);
 
 // Get 1 user information from token 
-router.get('/users', permit(), getUser);
+router.get('/user', permit(), getUser);
 
 router.post('/room', createRoom);
 router.get('/rooms', getWaitingRooms);
